f1b2baff2cd90e23a3c08af51d8889244c3c0298
Michael Speed Elder <michael.speed.elder@gmail.com>
2024-01-09 13:53:26 -0600

    add demos with shorter hero images

--- demo/demo-scripts.js
+++ demo/demo-scripts.js
@@ -1,5 +1,7 @@
 'use strict'
 ;(() => {
+  const addAdIndex = () => {}
+
   const bindOnAnimEnd =
     (/** @type {Element} */ el) => (/** @type {AnimationEvent} */ evt) => {
       if (evt.animationName === 'appear') {
@@ -10,5 +12,12 @@
 
   document.querySelectorAll('.ad-row').forEach((el) => {
     el.addEventListener('animationend', bindOnAnimEnd(el))
+  })
+
+  document.querySelectorAll('.ads').forEach((ads) => {
+    let i = 1
+    ads.querySelectorAll('.ad').forEach((ad) => {
+      ad.setAttribute('style', `--animation-index: ${i++}`)
+    })
   })
 })()
