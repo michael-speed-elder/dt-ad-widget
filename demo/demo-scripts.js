075233fd397a10a21355151a012d440840793d8f
Michael Speed Elder <michael.speed.elder@gmail.com>
2024-01-09 12:06:15 -0600

    fix scroll-snap

--- demo/demo-scripts.js
+++ demo/demo-scripts.js
@@ -1,3 +1,14 @@
 'use strict'
+;(() => {
+  const bindOnAnimEnd =
+    (/** @type {Element} */ el) => (/** @type {AnimationEvent} */ evt) => {
+      if (evt.animationName === 'appear') {
+        el.classList.add('scroll-snap')
+        el.removeEventListener('animationend', bindOnAnimEnd)
+      }
+    }
 
-;(() => {})()
+  document.querySelectorAll('.ad-row').forEach((el) => {
+    el.addEventListener('animationend', bindOnAnimEnd(el))
+  })
+})()
