a860d694c1711418a4743225133b82a0837524bf
Michael Speed Elder <michael.speed.elder@gmail.com>
2024-01-12 15:28:00 -0600

    add imports for static image data

--- demo/data.js
+++ demo/data.js
@@ -1,27 +1,33 @@
+import maxSrc from './images/max-hero.webp'
+import robloxSrc from './images/roblox.webp'
+import tubiSrc from './images/tubi.png'
+import royalMatchSrc from './images/royal_match.webp'
+import monopSrc from './images/monop-go.webp'
+
 export default [
   {
     title: 'Max',
     desc: 'Enjoy your favorite shows and movies at home or on-the-go. Family-friendly entertainment for the whole household.',
-    src: './images/max-hero.webp',
+    src: maxSrc,
   },
   {
     title: 'Max',
     desc: 'Roblox: let the games begin! Install instantly!',
-    src: './images/roblox.webp',
+    src: robloxSrc,
   },
   {
     title: 'Max',
     desc: 'Enjoy movies and TV series or watch live TV—for free. No subscription.',
-    src: './images/tubi.png',
+    src: tubiSrc,
   },
   {
     title: 'Max',
     desc: 'Royal Match, the king of puzzle games — Swipe colors, solve puzzles!',
-    src: './images/royal_match.webp',
+    src: royalMatchSrc,
   },
   {
     title: 'Max',
     desc: 'Roll the dice and get rich in Monopoly GO!',
-    src: './images/monop-go.webp',
+    src: monopSrc,
   },
 ]
